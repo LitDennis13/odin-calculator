b552b656b955cf87908a5e11b95f80ae60eaab46
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2023-11-19 20:46:10 -0600

    make all non opperation buttons functional

---
+++ calculate.js
@@ -0,0 +1,15 @@
+let oppButtons = document.querySelector("#operation-buttons");
+
+let expression = {
+    firstNumber: 0,
+    secondNumber: 0,
+    operation: "+"
+};
+
+
+
+
+oppButtons.addEventListener("click", function(event) {
+    let target = event.target;
+    
+});
--- script.js
+++ script.js
@@ -2,14 +2,83 @@ let listOfNumbers = [0,1,2,3,4,5,6,7,8,9];
 
 
 let display = document.querySelector("#display");
-let buttons = document.querySelectorAll(".clear, .plus-minus, .percent, .seven, .eight, .nine, .four, .five, .six, .one, .two, .three, .zero, .period, .equals");
-let operationButtons = document.querySelectorAll(".multiplication, .division, .subtraction, .addition");
+let nonOppButtons = document.querySelector("#non-operation-buttons");
 
 
-buttons.addEventListener("click", function(event) {
+let removedZero = false;
+
+
+nonOppButtons.addEventListener("click", function(event) {
     let target = event.target;
-    console.log(target);
-    if (listOfNumbers.includes(+target.textContent)) {
+    
+    if (target.textContent === "0" && display.textContent === "0") {
+        return;
+    }
+
+    else if (listOfNumbers.includes(+target.textContent)) {
+        removeZero();
         display.textContent += target.textContent;
     }
+
+
+    else {
+        switch (target.classList[0]) {
+            case "clear":
+                clear();
+                break;
+
+            case "plus-minus":
+                plus_minus();
+                break;
+                                
+            case "percent":
+                percent();
+                break;
+                                
+            case "period":
+                decimal();
+                break;
+        }
+    }
 });
+function removeZero() {
+    if (!removedZero) {
+        display.textContent = "";
+        removedZero = true;
+    }
+}
+
+
+function clear() {
+    removedZero = false;
+    display.textContent = "0";
+    expression = {
+        firstNumber: 0,
+        secondNumber: 0,
+        operation: "+"
+    };
+}
+
+function plus_minus() {
+    if (display.textContent === "0." || +display.textContent === 0) {
+        display.textContent = "0";
+        removedZero = false;
+    }
+    else if (display.textContent.includes("-")) {
+        display.textContent = display.textContent.substring(1);
+    }
+    else {
+        display.textContent = "-"+display.textContent;
+    }
+}
+
+function percent() {
+    display.textContent = (+display.textContent / 100);
+}
+
+function decimal() {
+    if (!display.textContent.includes(".")) {
+        display.textContent += ".";
+        removedZero = true;
+    }
+}
